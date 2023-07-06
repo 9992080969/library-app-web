544e2a8e4d450ae59b958f319513c9a88675896d
Alexey Voronov <9992080969@proton.me>
2023-07-06 17:06:35 +0300

    books + people

--- src/main/java/com/library/app/dao/BookDAO.java
+++ src/main/java/com/library/app/dao/BookDAO.java
@@ -16,7 +16,26 @@ import java.util.List;
 public class BookDAO {
     private final JdbcTemplate jdbcTemplate;
 
-    public List<Book> index(){
+    public List<Book> index() {
         return jdbcTemplate.query("SELECT * FROM books", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM books WHERE books_id=?",
+                        new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO books(title, author, bookyear) VALUES (?,?,?)",
+                book.getTitle(), book.getAuthor(), book.getBookYear());
+    }
+
+    public void update(int id, Book updateBook) {
+        jdbcTemplate.update("UPDATE books SET title=?, author=?, bookyear=? WHERE books_id=?",
+                updateBook.getTitle(), updateBook.getAuthor(), updateBook.getBookYear(), id);
+    }
+    public void delete(int id){
+        jdbcTemplate.update("DELETE FROM books WHERE books_id=?", id);
     }
 }
